32bfdc24cd3480e0a856f98d4077a1fb777b090b
IceParazit <34073415+IceParazit@users.noreply.github.com>
2023-11-22 22:30:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import smtplib # Модуль для работы с почтой
 # Основной класс
 class Currency:
 	# Ссылка на нужную страницу
-	DOLLAR_EURO = 'https://www.google.com/search?q=%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80+%D0%BA+%D0%B5%D0%B2%D1%80%D0%BE&sca_esv=584647371&sxsrf=AM9HkKkaMc57VbdQh04xiiHJNsw9s_0Qgg%3A1700678149985&ei=BUpeZZfbO7e9i-gP1I2vqAc&ved=0ahUKEwjX8Ybon9iCAxW33gIHHdTGC3UQ4dUDCBA&uact=5&oq=%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80+%D0%BA+%D0%B5%D0%B2%D1%80%D0%BE&gs_lp=Egxnd3Mtd2l6LXNlcnAiGNC00L7Qu9C70LDRgCDQuiDQtdCy0YDQvjIKEAAYgAQYRhiCAjIFEAAYgAQyChAAGIAEGBQYhwIyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAESLcTUABYsRBwAngBkAEAmAGcAaABiQiqAQMwLji4AQPIAQD4AQHCAgcQABiABBgKwgIMEC4YFhgeGMcBGNEDwgIGEAAYFhgewgIJEAAYgAQYChgqwgIIEAAYFhgeGArCAg4QLhgWGB4YxwEY0QMYCsICCxAAGBYYHhjxBBgKwgIKECMYgAQYigUYJ-IDBBgAIEGIBgE&sclient=gws-wiz-serp'
+	DOLLAR_EURO = 'https://www.google.com/search?q=%D0%B5%D0%B2%D1%80%D0%BE+%D0%BA+%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80%D1%83&sca_esv=584647371&sxsrf=AM9HkKkQXe9-1NHHGGmT31ravcTwd6hn5w%3A1700679294568&ei=fk5eZfKEIqXQi-gP_a-40AE&oq=%D0%B5%D0%B2%D1%80%D0%BE+&gs_lp=Egxnd3Mtd2l6LXNlcnAiCdC10LLRgNC-ICoCCAAyDxAAGIAEGIoFGEMYRhiCAjIKEAAYgAQYigUYQzIKEAAYgAQYigUYQzIKEAAYgAQYigUYQzIKEAAYgAQYigUYQzIKEAAYgAQYigUYQzIKEAAYgAQYigUYQzIKEAAYgAQYigUYQzIKEAAYgAQYigUYQzIKEAAYgAQYigUYQ0iBGlAAWKIPcAJ4AZABAJgBnAGgAawHqgEDMC43uAEDyAEA-AEBqAIQwgIKECMYgAQYigUYJ8ICBRAAGIAEwgILEC4YgAQYxwEY0QPCAhAQABgBGIAEGIoFGAoYQxgqwgILEC4YrwEYxwEYgATCAgsQLhiABBjHARivAcICBxAjGOoCGCfCAhQQABiABBjjBBjpBBjqAhi0AtgBAcICBBAjGCfiAwQYACBBiAYBugYGCAEQARgB&sclient=gws-wiz-serp'
 	# Заголовки для передачи вместе с URL
 	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'}
 
@@ -31,13 +31,7 @@ class Currency:
 
 	# Проверка изменения валюты
 	def check_currency(self):
-		currency = float(self.get_currency_price().replace(",", "."))
-		if currency >= self.current_converted_price + self.difference:
-			print("Курс сильно вырос, может пора что-то делать?")
-		elif currency <= self.current_converted_price - self.difference:
-			print("Курс сильно упал, может пора что-то делать?")
-
-		print("Сейчас курс: 1 доллар = " + str(currency), "Евро")
+		print("Сейчас разница евродоллар " + str(currency))
 
 
 
